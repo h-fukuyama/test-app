4b1bfead8a8e1ec352d928781e7095fe0f0b0dcd
h-fukuyama <h-fukuyama@usen-next.jp>
2024-01-16 10:57:27 +0900

    bgmBand修正、ルール7完了

--- src/utils/bgmBand.js
+++ src/utils/bgmBand.js
@@ -1,12 +1,12 @@
 export const processBGMBand = (firstTwoDigits) => {
     if (firstTwoDigits >= '41' && firstTwoDigits <= '5A') {
-      return String.fromCharCode(parseInt(firstTwoDigits, 16) - 0x40);
+      return String.fromCharCode(parseInt(firstTwoDigits, 16));
     } else if (firstTwoDigits >= '5B' && firstTwoDigits <= '74') {
       // UA~UZ
-      return 'U' + String.fromCharCode(parseInt(firstTwoDigits, 16) - 0x5A);
+      return 'U' + String.fromCharCode(parseInt(firstTwoDigits, 16) - 26);
     } else if (firstTwoDigits >= '75' && firstTwoDigits <= '8E') {
       // ZA~ZZ
-      return 'Z' + String.fromCharCode(parseInt(firstTwoDigits, 16) - 0x74);
+      return 'Z' + String.fromCharCode(parseInt(firstTwoDigits, 16) - 52);
     } else if (firstTwoDigits === 'FF') {
       // AUX
       return 'AUX';
