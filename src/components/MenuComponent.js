e3e63e2254cef3e355b2d096e9c63f556ca92b37
h-fukuyama <h-fukuyama@usen-next.jp>
2024-02-19 10:52:13 +0900

    ひな型作成

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -15,6 +15,25 @@ const MenuComponent = () => {
     }
   }, [file, navigate]);
 
+  const MenuProcessor = ({ menu }) => {
+    const menuPropertyFunctions = [
+      processFunction1, processFunction2, processFunction3, processFunction4, processFunction5, processFunction6, processFunction7, processFunction8, processFunction9, processFunction10,
+      processFunction11, processFunction12, processFunction13, processFunction14, processFunction15, processFunction16, processFunction17
+    ];
+
+    const results = [];
+
+    for (let i = 0; i < 17 ; i++) {
+      const property = menu[i];
+      const func = menuPropertyFunctions[i];
+
+      const result = func(property);
+
+      results.push(result);
+    }
+    return results;
+  };
+
   return (
     <div>
       {file && ( //fileが存在すれば以下を表示
